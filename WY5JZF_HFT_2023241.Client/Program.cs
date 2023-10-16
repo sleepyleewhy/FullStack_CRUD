d4e6a369e9c1e22725eb0ee669ed8c8dcb2cd723
endll <endll@ASUS_LEVI>
2023-10-16 11:44:06 +0200

    Add project files.

---
+++ WY5JZF_HFT_2023241.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WY5JZF_HFT_2023241.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WY5JZF_HFT_2023241.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WY5JZF_HFT_2023241.Models
+{
+    public class Class1
+    {
+    }
+}
