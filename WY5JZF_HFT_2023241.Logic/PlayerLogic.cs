8ffb856ffdfb4b1809ba349358887ed44345dbfe
Endl Levente <endl.levente7@gmail.com>
2023-11-06 17:55:07 +0100

    PlayerLogic created.

---
+++ WY5JZF_HFT_2023241.Logic/PlayerLogic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using WY5JZF_HFT_2023241.Models;
+using WY5JZF_HFT_2023241.Repository;
+
+namespace WY5JZF_HFT_2023241.Logic
+{
+    public class PlayerLogic
+    {
+        IRepository<Player> repo;
+        public PlayerLogic(IRepository<Player> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Player item)
+        {
+            if (item.PlayerName.Length > 240 || item.Position < 1 || item.Position > 5)
+            {
+                throw new ArgumentException("Invalid Player!");
+            }
+            else
+            {
+                repo.Create(item);
+            }
+        }
+        public Player Read(int id)
+        {
+            var item = repo.Read(id);
+            if (item == null)
+            {
+                throw new ArgumentException("Player doesn't exist");
+            }
+            else
+            { return item; }
+        }
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+        public IEnumerable<Player> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+        public void Update(Player item)
+        {
+            repo.Update(item);
+        }
+
+    }
+}
---
+++ WY5JZF_HFT_2023241.Logic/TeamLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WY5JZF_HFT_2023241.Models;
+using WY5JZF_HFT_2023241.Repository;
+
+namespace WY5JZF_HFT_2023241.Logic
+{
+    public class TeamLogic
+    {
+        IRepository<Team> repo;
+        public TeamLogic(IRepository<Team> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Team item)
+        {
+
+        }
+    }
+}
