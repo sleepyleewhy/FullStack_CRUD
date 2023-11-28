549ee37a144cf3b4a533dbe7627092aadad0c792
Endl Levente <endl.levente7@gmail.com>
2023-11-28 22:07:16 +0100

    Listing created to Console

--- WY5JZF_HFT_2023241.Client/Program.cs
+++ WY5JZF_HFT_2023241.Client/Program.cs
@@ -1,18 +1,23 @@
 ﻿using ConsoleTools;
 using System;
+using System.Collections.Generic;
 using WY5JZF_HFT_2023241.Models;
 
 namespace WY5JZF_HFT_2023241.Client
 {
     internal class Program
     {
+
+        static RestService rest;
         static void Main(string[] args)
         {
+
+            rest = new RestService("http://localhost:40930/", "division");
             var divisionSubMenu = new ConsoleMenu(args, level: 1)
-                .Add("List", () => List("Director"))
-                .Add("Create", () => Create("Director"))
-                .Add("Delete", () => Delete("Director"))
-                .Add("Update", () => Update("Director"))
+                .Add("List", () => List("Division"))
+                .Add("Create", () => Create("Division"))
+                .Add("Delete", () => Delete("Division"))
+                .Add("Update", () => Update("Division"))
                 .Add("Exit", ConsoleMenu.Close);
 
             var teamSubMenu = new ConsoleMenu(args, level: 1)
@@ -42,13 +47,39 @@ namespace WY5JZF_HFT_2023241.Client
         {
             if (entity == "Division")
             {
-
+                List<Division> divisions = rest.Get<Division>("division");
+                foreach (Division division in divisions)
+                {
+                    Console.WriteLine(division.DivisionName);
+                }
             }
+            else if (entity == "Team")
+            {
+                List<Team> teams = rest.Get<Team>("team");
+                foreach (Team item in teams)
+                {
+                    Console.WriteLine(item.TeamName);
+                }
+            }
+            else if (entity == "Player")
+            {
+                List<Player> players = rest.Get<Player>("player");
+                foreach(Player player in players)
+                {
+                    Console.WriteLine(player.PlayerName);
+                }
+            }
+
             Console.ReadLine();
         }
         static void Update(string entity)
         {
             Console.WriteLine(entity+ " update");
+            if (entity == "Division")
+            {
+                rest.Put<Division>()
+            }
+
             Console.ReadLine();
         }
         static void Delete(string entity)
